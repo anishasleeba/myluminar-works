6e36d2d356b45c3c43534522f6f4f5c87bd2ebb1
Anisha <anisha.sleeba@gmail.com>
2021-06-10 11:06:13 +0530

    all changes

---
+++ javascript/javafund.js
@@ -0,0 +1,11 @@
+//var age= 27;
+//var p_name="Anish Sleeba";
+//console.log("Hai am "+p_name+" "+age+" years old.");
+/*var state="Kerala";
+var active_cases = 15000;
+var cured = 20000;
+var death = 200;
+console.log(`In ${state} ${active_cases} new cases and ${cured} cured death ${cured} had been reported`)*/
+var name="anisha";
+console.log(typeof(name));
+
