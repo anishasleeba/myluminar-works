9ad5993b0d9e22f8b780f70760f96d5d0644362e
Anisha <anisha.sleeba@gmail.com>
2021-07-28 16:47:23 +0530

    updates

---
+++ New folder/test.js
@@ -0,0 +1,49 @@
+// var a, x, d = 0;
+// var b = 153
+// x=b;
+
+// while (x > 0) {
+//   a = (x % 10);
+//   x = Math.floor(x/10);
+//   d = d + (a*a*a);
+// }
+
+// if (b==d){
+//    console.log("given number is an armstrong number");}
+// else
+//    {
+//     console.log("given number is not an armstrong number");
+//    }
+
+
+
+// function reverseString(str) {
+//    var newString = "";
+//    for (let i = str.length - 1; i >= 0; i--) {
+//        newString += str[i];
+//    }
+//    return newString;
+// }
+// console.log(reverseString('hello'));
+
+
+// var sum=0;
+// var a=[50,50,50,50,50];
+// for(let i in a){
+//    sum+=a[i]
+// }
+// console.log(sum/5);
+
+
+// let n = 5; 
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += "*";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+   
